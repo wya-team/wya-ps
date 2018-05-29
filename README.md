46e2331e262cca726de1320bd932d829035d9bc0
Deot <zrd0921@qq.com>
2018-05-29 12:19:29 +0800

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# wya-ps
+<!--  以下内容无视  -->
+## [Demo](https://wya-team.github.io/wya-ps/demo/index.html)
---
+++ src/main.js
@@ -0,0 +1,16 @@
+/**
+ * 微型事件Pub/Sub模块
+ */
+
+class Publish {
+	constructor() {
+	}
+	on() {
+	}
+	trigger() {
+
+	}
+	off() {
+
+	}
+}
